57c84006dfa2f282824af266790cb75a32e5a528
Adnan Rasheed <dannykhan058@gmail.com>
2023-02-21 15:22:17 +0500

    done

--- slider/slider.js
+++ slider/slider.js
@@ -1,26 +1,36 @@
-const slides = document.querySelectorAll(".slide")
-var counter = 0;
-// console.log(slides)
-slides .forEach(
-   (slide, index)  => {
-    slide.style.top = `${index * 100}%`
-}
-)
-const goPrev = () =>{
-    counter --
-    slideImage()
-}
-const goNext = () =>{
-    counter ++
-    slideImage()
-}
+// const slides = document.querySelectorAll(".slide")
+// var counter = 0;
+// // console.log(slides)
+// slides .forEach(
+//    (slide, index)  => {
+//     slide.style.top = `${index * 100}%`
+// }
+// )
+// const goPrev = () =>{
+//     counter --
+//     slideImage()
+// }
+// const goNext = () =>{
+//     counter ++
+//     slideImage()
+// }
 
-const slideImage = () => {
-    slides.forEach(
-        (slide) => {
-            slide.style.transform = `translatey(-${counter * 100}%)`
+// const slideImage = () => {
+//     slides.forEach(
+//         (slide) => {
+//             slide.style.transform = `translatey(-${counter * 100}%)`
 
-        }
-    )
+//         }
+//     )
 
-}
+// }
+let text1 ="danny";
+let text2 ="danny";
+document.write(text1 );
+document.write("<br>")
+document.write("<br>")
+
+let str = "JavaScript is a Good language";
+let length = str.search("is");
+document.write(length)
+
